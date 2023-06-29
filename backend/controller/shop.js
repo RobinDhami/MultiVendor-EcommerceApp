ebb94313f2e33a96a41e59e3697063cd21f8f405
RobinDhami <rozendhami69@gmail.com>
2023-06-29 00:30:28 -0700

    shop backend url

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -44,7 +44,7 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://multivendorecomapp-rozen.vercel.app/seller/activation/${activationToken}`;
+    const activationUrl = `https://multivendorecomapp-rozen-cefl.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
