4e024a915645000caef9412469d9bc983feb061e
RobinDhami <rozendhami69@gmail.com>
2023-06-29 00:13:11 -0700

    user url

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -40,7 +40,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://multivendorecomapp-rozen-cefl.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://multivendorecomapp-rozen.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
