f61e00965d63bb9b776d89410a79d97b5a8e3bd6
RobinDhami <rozendhami69@gmail.com>
2023-06-28 23:14:25 -0700

    origin url app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://multivendorecomapp-rozen-cefl.vercel.app/',
   credentials: true
 }));
 
